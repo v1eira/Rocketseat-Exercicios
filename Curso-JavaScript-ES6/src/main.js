47458daa5daf9cee3c5ecccdbcfa33362dc2052e
v1eira <ewersonv@gmail.com>
2019-06-05 14:53:41 -0300

    parte 4 incompleto

--- Curso-JavaScript-ES6/src/main.js
+++ Curso-JavaScript-ES6/src/main.js
@@ -1,16 +1,56 @@
-import axios from 'axios';
+class App {
+    constructor() {
+        this.repositories = [];
 
-class Api {
-    static async getUserInfo(username) {
-        try {
-            const response = await axios.get(`https://api.github.com/users/${username}`);
+        this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
 
-            console.log(response);
-        } catch (err) {
-            console.warn('Erro na API');
-        }
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    addRepository(event) {
+        event.preventDefault();
+        
+        this.repositories.push({
+            name: 'Ewerson Vieira Nascimento',
+            description: 'Estudante de Sistemas de Informação do Ifes, campus Serra.',
+            avatar_url: 'https://avatars2.githubusercontent.com/u/22565566?s=460&v=4',
+            html_url: 'https://github.com/v1eira'
+        });
+
+        this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
 
-Api.getUserInfo('v1eira');
-Api.getUserInfo('ewersonv');
+new App();
